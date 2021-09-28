1c363e206d5cc007a00a677433fcdcaebc839be8
Jaime-Gabriel-Hernandez-Garcia <jhernandezg1915@alumno.ipn.mx>
2021-09-28 15:24:24 -0500

    Algoritmo Vigenere

---
+++ 02Vigenere/JS/algoritmoVigenere.js
@@ -0,0 +1,22 @@
+/*
+Vamos a realiazar el algoritmo para cifrado viggener
+
+*/
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f',
+              'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'ñ']
+let key = "";
+
+$(document).ready(function(){
+    $('#ci').click(function(){
+
+        key= document.getElementById('llave').value;
+
+        key = key.replace(/ /g, '');
+
+        let mess = document.getElementById('mess').value;
+
+        mess = 
+    })
+})
